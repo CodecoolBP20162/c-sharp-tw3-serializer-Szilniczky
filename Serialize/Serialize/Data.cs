ec5767b904a4563c9460283f7c477ab67705778e
Szilniczky <szilniczky.david@gmail.com>
2017-07-30 19:11:57 +0200

    add files

---
+++ Serialize/Serialize/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialize
+{
+    [Serializable]
+    public class Data
+    {
+        private string name; 
+        private string address; 
+        private string phone;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+}
